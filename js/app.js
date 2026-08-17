403bd84cc0da6ea574beaee8a69bae35ade0180b
Romain <rmasson@edenschool.fr>
2026-08-17 16:58:55 +0200

    Exam-DOM-API-02

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 const form = document.querySelector("form");
-const title = document.querySelector("#titre");
-const URL = document.querySelector("#url");
+const title1 = document.querySelector("#titre");
+const url1 = document.querySelector("#url");
 const desc = document.querySelector("#description");
+
+const container = document.querySelector(".favoris-container");
 
 async function API() {
   const response = await fetch("http://10.69.4.8:8100/bookmarks", {
@@ -14,15 +16,32 @@ async function API() {
 
   const data = await response.json();
 
-  console.log(data);
+  return data;
+}
+
+function load(TabAPI) {
+  for (let i = 0; i < API.length; i++) {
+    const li = document.createElement("li");
+
+    const a = document.createElement("a");
+    a.textContent = TabAPI[i].title1.value;
+    a.setAttribute("href", TabAPI[i].URL.value);
+
+    const p = document.createElement("p");
+    p.textContent = TabAPI[i].desc.value;
+
+    li.append(a, p);
+    container.appendChild(li);
+
+    return container;
+  }
 }
 
 function favos() {
-  const container = document.querySelector(".favoris-container");
   const li = document.createElement("li");
 
   const a = document.createElement("a");
-  a.textContent = title.value;
+  a.textContent = title1.value;
   a.setAttribute("href", URL.value);
 
   const p = document.createElement("p");
@@ -34,11 +53,13 @@ function favos() {
   return container;
 }
 
-document.addEventListener("DOMContentLoaded", () => {});
+document.addEventListener("DOMContentLoaded", async () => {
+  const resAPI = await API();
+  load(resAPI);
+});
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   favos();
   API();
-  // console.log(title.value, URL.value, desc.value);
 });
